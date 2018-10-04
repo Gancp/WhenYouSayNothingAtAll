112310b70060c4c493a0cf9499af54dd6a882b74
lovegcp <1197879506@qq.com>
2018-10-04 17:16:29 +0800

    原型项目

---
+++ src/main/java/com/gancp/losesleep/LosesleepApplication.java
@@ -0,0 +1,12 @@
+package com.gancp.losesleep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LosesleepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LosesleepApplication.class, args);
+	}
+}
